b250eb174aaa8224b8182d883534b1972dfad4d5
olkola <juego@o2.pl>
2016-09-19 22:18:10 +0200

    Update script.js

--- jq/script.js
+++ jq/script.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
-$("span:odd").css('color','red');
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
-
-var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-$(element).append(button) 
-});
-$("button").click(function(){
+	$("span:odd").css('color','red');
+	var paragraphs = $('p');
+	paragraphs.each(function(index, element) {
+		var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+		$(element).append(button) 
+	});
+	$("button").click(function(){
 	alert($(this).attr("data-tmp"));
-});
+	});
 });
